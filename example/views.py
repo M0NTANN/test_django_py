ce6dd5adfad6a0f2bea06681f2ecb4a2762401fb
M0NTANN <129042407+M0NTANN@users.noreply.github.com>
2023-04-25 10:47:25 +0300

    Update views.py

--- example/views.py
+++ example/views.py
@@ -1,15 +1,17 @@
 # example/views.py
 from datetime import datetime
+import os
 
 from django.http import HttpResponse
 
 def index(request):
+    token = os.environ['TOKEN']
     now = datetime.now()
     html = f'''
     <html>
         <body>
             <h1>Hello from Verertererercel!</h1>
-            <p>The current time is { now }.</p>
+            <p>The current time is { now }.  Token: {token}</p>
         </body>
     </html>
     '''
